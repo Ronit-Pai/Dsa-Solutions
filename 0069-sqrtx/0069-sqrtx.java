c855fce1712e7a004eb419eb4981644b20f54ee3
Ronit Pai <ronit14pai06@gmail.com>
2026-07-08 19:21:12 +0530

    Time: 1 ms (97.62%), Space: 42.5 MB (61.16%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1)
+            return x;
+        int l = 1;
+        int h = x;
+        int mid = -1;
+        while (l <= h){
+            mid = l + (h - l) / 2;
+            if ((long) mid * mid > (long) x)
+                h = mid - 1;
+            else if (mid * mid == x)
+                return mid;
+            else
+                l = mid + 1;
+        }
+        return Math.round(h);
+    }
+}
